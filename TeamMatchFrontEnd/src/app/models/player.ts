d12bf82e2f806a8edb3e820ee719095de87b1bd6
DiegoJohnsonL <diegojohnson46@gmail.com>
2020-11-13 19:15:32 -0500

    create-player-service/model/component

---
+++ TeamMatchFrontEnd/src/app/models/player.ts
@@ -0,0 +1,14 @@
+export class Player {
+  username: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  description: string;
+  gender: string;
+  emailAddress: string;
+  phoneNumber: string;
+  birthDate: any;
+  level: number;
+  hoursPlayed: number;
+  killDeathRatio: number;
+}
