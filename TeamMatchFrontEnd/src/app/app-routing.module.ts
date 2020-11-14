847ef0777066612af547206d1173a4c7ae24e045
JoseMoreno21 <57198383+JoseMoreno21@users.noreply.github.com>
2020-11-14 13:48:57 -0500

    Organizer Endpoint
    Creación del organizer endpoint

--- TeamMatchFrontEnd/src/app/app-routing.module.ts
+++ TeamMatchFrontEnd/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {OrganizersComponent} from './page/organizers/organizers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'organizers', component: OrganizersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [OrganizersComponent];
--- TeamMatchFrontEnd/src/app/app.module.ts
+++ TeamMatchFrontEnd/src/app/app.module.ts
@@ -1,24 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {HttpClientModule} from '@angular/common/http';
 import { PlayersComponent } from './page/players/players.component';
 import { OrganizersComponent } from './page/organizers/organizers.component';
+import { TournamentsComponent } from './page/tournaments/tournaments.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayersComponent,
-    OrganizersComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
